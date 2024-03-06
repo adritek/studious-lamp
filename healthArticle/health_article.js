fa92a173f59a6b91ac8cf7572bd646102ce4c7f7
Adrian Rogers <adritek@gmail.com>
2024-03-06 10:52:37 -0500

    add second fetch todo: reduce dup code

--- healthArticle/health_article.js
+++ healthArticle/health_article.js
@@ -1,12 +1,17 @@
 var xhr = new XMLHttpRequest();
+var newsXhr = new XMLHttpRequest();
+
 var url = './health_article.json';
+var newsUrl = './health_article.json';
 
 xhr.open('GET', url, true);
+newsXhr.open('GET', newsUrl, true);
+
 xhr.responseType = 'json';
 
 xhr.onload = function(){
     var articles = xhr.response.articles;
-    var articlesDiv = document.getElementById('articles');
+    var articlesDiv = document.getElementById('healthArticles');
     
     articles.forEach(function(article) {
         var articleDiv = document.createElement('div');
@@ -48,5 +53,51 @@ xhr.onload = function(){
         articlesDiv.appendChild(articleDiv);
     });
 }
+
+newsXhr.onload = function(){
+    var articles = xhr.response.articles;
+    var articlesDiv = document.getElementById('newsArticles');
     
-    xhr.send();
+    articles.forEach(function(article) {
+        var articleDiv = document.createElement('div');
+        articleDiv.classList.add('article');
+        
+        var title = document.createElement('h2');
+        title.textContent = `NEWS ${article.title}`;
+        
+        var description = document.createElement('p');
+        description.textContent = article.description;
+        
+        var waysHeader = document.createElement('h3');
+        waysHeader.textContent = 'NEWS Ways to Achieve:';
+        
+        var waysList = document.createElement('ul');
+        article.ways_to_achieve.forEach(function(way) {
+            var listItem = document.createElement('li');
+            listItem.textContent = way;
+            waysList.appendChild(listItem);
+        });
+        
+        var benefitsHeader = document.createElement('h3');
+        benefitsHeader.textContent = 'NEWS Benefits:';
+        
+        var benefitsList = document.createElement('ul');
+        article.benefits.forEach(function(benefit) {
+            var listItem = document.createElement('li');
+            listItem.textContent = benefit;
+            benefitsList.appendChild(listItem);
+        });
+        
+        articleDiv.appendChild(title);
+        articleDiv.appendChild(description);
+        articleDiv.appendChild(waysHeader);
+        articleDiv.appendChild(waysList);
+        articleDiv.appendChild(benefitsHeader);
+        articleDiv.appendChild(benefitsList);
+        
+        articlesDiv.appendChild(articleDiv);
+    });
+}
+    
+xhr.send();
+newsXhr.send();
